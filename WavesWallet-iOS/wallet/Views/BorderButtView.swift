1aeab01d58e0d44d6e8b311254514adc3297befb
Pavel Gubin <xdozorx@gmail.com>
2018-07-11 11:56:07 +0300

    update backup screen

--- WavesWallet-iOS/wallet/Views/BorderButtView.swift
+++ WavesWallet-iOS/wallet/Views/BorderButtView.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class BorderButtView: UIView {
-
-    let lineColor: UIColor = UIColor.basic300
+    
+    var errorMode = false
     
     override func draw(_ rect: CGRect) {
-        
-        let path = UIBezierPath(roundedRect: bounds, cornerRadius: 3)
+
+        let path = UIBezierPath(roundedRect: CGRect(x: 0.5, y: 0.5, width: frame.size.width - 1, height: frame.size.height - 1), cornerRadius: 3)
         path.lineWidth = 0.5
         let dashes: [CGFloat] = [6, 4]
         path.setLineDash(dashes, count: dashes.count, phase: 0)
         path.lineCapStyle = CGLineCap.butt
-        lineColor.setStroke()
+        
+        if errorMode {
+            UIColor.error500.setStroke()
+        }
+        else {
+            UIColor.basic300.setStroke()
+        }
         path.stroke()
     }
     
