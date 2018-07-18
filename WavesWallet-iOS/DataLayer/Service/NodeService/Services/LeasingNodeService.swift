dbe3fa603abd07e07ab3e8e96b007d42d2457714
mefilt <rprokofev@wavesplatform.com>
2018-07-18 20:40:54 +0300

    WIP Wallet

---
+++ WavesWallet-iOS/DataLayer/Service/NodeService/Services/LeasingNodeService.swift
@@ -0,0 +1,9 @@
+//
+//  LeasingNodeService.swift
+//  WavesWallet-iOS
+//
+//  Created by mefilt on 18.07.2018.
+//  Copyright © 2018 Waves Platform. All rights reserved.
+//
+
+import Foundation
---
+++ WavesWallet-iOS/extensions/Type/SkeletonCell.swift
@@ -0,0 +1,9 @@
+//
+//  SkeletonCell.swift
+//  WavesWallet-iOS
+//
+//  Created by mefilt on 17.07.2018.
+//  Copyright © 2018 Waves Platform. All rights reserved.
+//
+
+import Foundation
