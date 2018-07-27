330f27ccdee449dce8f938fec79e38edf036993d
Pavel Gubin <xdozorx@gmail.com>
2018-07-27 04:25:16 +0300

    update

--- WavesWallet-iOS/Dex/DexDataHelper/DexListPresenter.swift
+++ WavesWallet-iOS/Dex/DexDataHelper/DexListPresenter.swift
@@ -10,107 +10,56 @@ import Foundation
 import SwiftyJSON
 import RxSwift
 
-
-enum DexDataState {
-    case isLoading
-    case normal
-}
-
 protocol DexListPresenterDelegate: class {
-    func dexListPresenter(listPresenter: DexListPresenter, didUpdateModels models: [DexListModel])
+    func dexListPresenter(listPresenter: DexListPresenter, didUpdateModels models: [DexTypes.DTO.DexListModel])
 }
 
-final class DexListPresenter: NSObject {
+final class DexListPresenter {
     
     var delegate: DexListPresenterDelegate?
-
-    private(set) var state = DexDataState.isLoading
-    private let interactor = DexInteractor()
-    private var items: [DexListModel] = []
-    private var tableView: UITableView!
-    private var numbOfRows : Int = 0
     
-    var models: [DexListModel] {
-        return items
-    }
-    
+    private let interactor : DexInteractorProtocol = DexInteractorMock()
+    private(set) var state = DexTypes.State.isLoading
+    private(set) var models: [DexTypes.DTO.DexListModel] = []
     private let bag = DisposeBag()
 
-    func setupTable(_ tableView: UITableView) {
-        self.tableView = tableView
-        tableView.dataSource = self
-        
-        interactor.dexPairs().subscribe(onNext: { (result) in
-            self.numbOfRows = result.count
-            
-        }, onError: nil, onCompleted: nil, onDisposed: nil).disposed(by: bag)
-    }
-    
-    func numberOfRouw
     //MARK: - TableData
     
-    func modelForIndexPath(_ indexPath: IndexPath) -> DexListModel {
+    func modelForIndexPath(_ indexPath: IndexPath) -> DexTypes.DTO.DexListModel {
         return models[indexPath.row]
     }
-    
-    var countSections: Int {
-        if state == .isLoading {
-            return 1
-        }
-        return 2
-    }
-    
+ 
     var numberOfRows: Int {
         if state == .isLoading {
             return 4
         }
         return models.count
     }
+    
+    func setupObservable() {
+        self.interactor.dexPairs().subscribe(onNext: { (result) in
+            self.state = .normal
+            self.models = result
+            self.delegate?.dexListPresenter(listPresenter: self, didUpdateModels: self.models)
+        }, onError: nil, onCompleted: nil, onDisposed: nil).disposed(by: self.bag)
+    }
 }
 
-//MARK: - UITableViewDataSource
-extension DexListPresenter: UITableViewDataSource {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return countSections
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return numberOfRows
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        if state == .isLoading {
-            let cell = tableView.dequeueCell() as DexListSkeletonCell
-            cell.slide(to: .right)
-            return cell
-        }
-        
-        let cell: DexListCell = tableView.dequeueCell()
-        cell.setupCell(modelForIndexPath(indexPath))
-        return cell
-    }
-
-}
 
 //MARK: - Test Data
 
 extension DexListPresenter {
     
     func simulateDataFromServer() {
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            self.initTestData()
-            self.state = .normal
-            self.tableView.reloadData()
-            self.delegate?.dexListPresenter(listPresenter: self, didUpdateModels: self.models)
+            self.interactor.dexPairs().subscribe(onNext: { (result) in
+                self.state = .normal
+                self.models = result
+                self.delegate?.dexListPresenter(listPresenter: self, didUpdateModels: self.models)
+            }, onError: nil, onCompleted: nil, onDisposed: nil).disposed(by: self.bag)
         }
     }
-    
-    private func initTestData() {
-        for _ in 0..<10 {
-            items.append(DexListModel(json: JSON()))
-        }
-    }
+  
     
 }
