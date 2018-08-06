684beec4eb6038df708aa0ae46be249cd9f49bc0
mefilt <rprokofev@wavesplatform.com>
2018-08-06 19:00:59 +0300

    WIP Assets

---
+++ WavesWallet-iOS/PresentationLayer/Asset/AssetViewPresenterProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  AssetViewPresenterProtocol.swift
+//  WavesWallet-iOS
+//
+//  Created by mefilt on 06.08.2018.
+//  Copyright © 2018 Waves Platform. All rights reserved.
+//
+
+import Foundation
