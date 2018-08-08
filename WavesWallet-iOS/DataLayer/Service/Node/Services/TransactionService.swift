f67886e11afb5765a5c488c2de3b13bbd135be99
mefilt <mefilt@gmail.com>
2018-08-08 10:37:34 +0300

    Added transactions servise

---
+++ WavesWallet-iOS/DataLayer/Service/Node/Services/TransactionService.swift
@@ -0,0 +1,56 @@
+//
+//  TransactionsService.swift
+//  WavesWallet-iOS
+//
+//  Created by Prokofev Ruslan on 07/08/2018.
+//  Copyright © 2018 Waves Platform. All rights reserved.
+//
+
+import Foundation
+
+extension Node.Service {
+    enum Transaction {
+        /**
+         Response:
+         - [Node.Model.LeasingTransaction].self
+         */
+        case list(accountAddress: String, limit: Int)
+        case info(id: String)
+    }
+}
+
+extension Node.Service.Transaction: NodeTargetType {
+    var modelType: Encodable.Type {
+        return String.self
+    }
+
+    private enum Constants {
+        static let transaction = "transaction"
+        static let limit = "limit"
+        static let address = "address"
+        static let info = "info"
+    }
+
+    var path: String {
+        switch self {
+        case .list(let accountAddress, let limit):
+            return Constants.transaction + "/" + Constants.address + "/" + "\(accountAddress)".urlEscaped + "/" + Constants.limit + "/" + "\(limit)".urlEscaped
+        case .info(let id):
+            return Constants.transaction + "/" + Constants.info + "/" + "\(id)".urlEscaped
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .list, .info:
+            return .get
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case .list, .info:
+            return .requestPlain
+        }
+    }
+}
