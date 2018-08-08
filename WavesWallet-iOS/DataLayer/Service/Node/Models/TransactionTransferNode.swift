540e2bb0b0c2b9b6688887f0f97305defd21c39c
mefilt <mefilt@gmail.com>
2018-08-08 10:37:34 +0300

    Added transactions servise

---
+++ WavesWallet-iOS/DataLayer/Service/Node/Models/TransactionTransferNode.swift
@@ -0,0 +1,27 @@
+//
+//  TransactionTransferNode.swift
+//  WavesWallet-iOS
+//
+//  Created by Prokofev Ruslan on 07/08/2018.
+//  Copyright © 2018 Waves Platform. All rights reserved.
+//
+
+import Foundation
+
+struct TransactionTransferNode {
+    let type: Int
+    let id: String
+    let sender: String
+    let senderPublicKey: String
+    let fee: Int64
+    let timestamp: Int64
+    let signature: String
+    let version: Int
+    let recipient: String
+    let assetID: String?
+    let feeAssetID: String?
+    let feeAsset: Int64?
+    let amount: Int64
+    let attachment: String
+    let height: Int64
+}
