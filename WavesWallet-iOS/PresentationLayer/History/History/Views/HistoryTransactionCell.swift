2bc216a9f65b0b98baedb595754e569e5205d769
ermolenkoj <elliottheproducer@gmail.com>
2018-08-22 19:49:35 +0400

    clean code

--- WavesWallet-iOS/PresentationLayer/History/History/Views/HistoryTransactionCell.swift
+++ WavesWallet-iOS/PresentationLayer/History/History/Views/HistoryTransactionCell.swift
@@ -18,3 +18,18 @@ final class HistoryTransactionCell: UITableViewCell, Reusable {
     
 }
 
+extension HistoryTransactionCell: ViewConfiguration {
+    
+    func update(with model: HistoryTypes.DTO.Transaction) {
+        transactionView.update(with: HistoryTransactionView.Transaction(with: model))
+    }
+    
+}
+
+fileprivate extension HistoryTransactionView.Transaction {
+    init(with transaction: HistoryTypes.DTO.Transaction) {
+        let kind = HistoryTransactionView.Transaction.Kind(rawValue: transaction.kind.rawValue)!
+        
+        self.init(id: transaction.id, name: transaction.name, balance: transaction.balance, kind: kind, tag: transaction.tag, date: transaction.date)
+    }
+}
