258b3827ae4e0aa4f665c16ae896d4f46b30e510
Pavel Gubin <xdozorx@gmail.com>
2018-08-27 02:45:29 +0300

    update

--- WavesWallet-iOS/dex/DexMyOrders/Interactor/DexMyOrdersInteractorMock.swift
+++ WavesWallet-iOS/dex/DexMyOrders/Interactor/DexMyOrdersInteractorMock.swift
@@ -25,16 +25,24 @@ final class DexMyOrdersInteractorMock: DexMyOrdersInteractorProtocol {
                 let date2 = Calendar.current.date(byAdding: .day, value: 2, to: today)!
                 let date3 = Calendar.current.date(byAdding: .day, value: 3, to: today)!
 
+                var statuses: [DexMyOrders.DTO.Status] = [.accepted,
+                                                          .partiallyFilled,
+                                                          .cancelled,
+                                                          .filled]
+                
                 for _ in 0..<10 {
-                    orders.append(DexMyOrders.DTO.Order(time: date1.addingTimeInterval(Double(arc4random() % 1000)), status: "dsda", price: Money(Double(arc4random() % 200)), amount: Money(Double(arc4random() % 300)), type: arc4random() % 2 == 0 ? .sell : .buy))
+                    let status = statuses[Int(arc4random() % UInt32(statuses.count))]
+                    orders.append(DexMyOrders.DTO.Order(time: date1.addingTimeInterval(Double(arc4random() % 1000)), status: status, price: Money(Double(arc4random() % 200)), amount: Money(Double(arc4random() % 300)), type: arc4random() % 2 == 0 ? .sell : .buy))
                 }
                 
                 for _ in 0..<10 {
-                    orders.append(DexMyOrders.DTO.Order(time: date2.addingTimeInterval(Double(arc4random() % 2000)), status: "gfdg", price: Money(Double(arc4random() % 200)), amount: Money(Double(arc4random() % 300)), type: arc4random() % 2 == 0 ? .sell : .buy))
+                    let status = statuses[Int(arc4random() % UInt32(statuses.count))]
+                    orders.append(DexMyOrders.DTO.Order(time: date2.addingTimeInterval(Double(arc4random() % 2000)), status: status, price: Money(Double(arc4random() % 200)), amount: Money(Double(arc4random() % 300)), type: arc4random() % 2 == 0 ? .sell : .buy))
                 }
                 
                 for _ in 0..<10 {
-                    orders.append(DexMyOrders.DTO.Order(time: date3.addingTimeInterval(Double(arc4random() % 3000)), status: "xxxfa", price: Money(Double(arc4random() % 200)), amount: Money(Double(arc4random() % 300)), type: arc4random() % 2 == 0 ? .sell : .buy))
+                    let status = statuses[Int(arc4random() % UInt32(statuses.count))]
+                    orders.append(DexMyOrders.DTO.Order(time: date3.addingTimeInterval(Double(arc4random() % 3000)), status: status, price: Money(Double(arc4random() % 200)), amount: Money(Double(arc4random() % 300)), type: arc4random() % 2 == 0 ? .sell : .buy))
                 }
                 
                 subscribe.onNext(orders)
