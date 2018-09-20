e4b0fa2af315eeba96e74586481eb6e1178e7af2
Pavel Gubin <xdozorx@gmail.com>
2018-09-20 23:53:11 +0300

    add order complete

---
+++ WavesWallet-iOS/dex/DexCompleteOrder/DexCompleteOrderViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DexCompleteOrderViewController.swift
+//  WavesWallet-iOS
+//
+//  Created by Pavel Gubin on 9/20/18.
+//  Copyright © 2018 Waves Platform. All rights reserved.
+//
+
+import UIKit
+
+final class DexCompleteOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
