9c8ae64d94e38f69a5cfb3a2b19dcfe5403975ad
Pavel Gubin <xdozorx@gmail.com>
2018-10-02 21:07:44 +0300

    d

--- WavesWallet-iOS/PresentationLayer/Receive/ReceiveContainerViewController.swift
+++ WavesWallet-iOS/PresentationLayer/Receive/ReceiveContainerViewController.swift
@@ -8,23 +8,83 @@
 
 import UIKit
 
+private enum SelectedState: Int {
+    case cryptoCurrency
+    case invoive
+    case card
+}
+
 final class ReceiveContainerViewController: UIViewController {
 
     private var viewControllers: [UIViewController] = []
     
     @IBOutlet private weak var segmentedControl: SegmentedControl!
     
-    private var selectedIndex: Int = 0
+    private var selectedState = SelectedState.cryptoCurrency
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = Localizable.Receive.Label.receive
         createBackButton()
+        setupSegmentedControl()
+        
+    }
+  
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupBigNavigationBar()
+    }
+}
+
+//MARK: - Actions
+private extension ReceiveContainerViewController {
+    
+    @IBAction func segmentedDidChange(_ sender: Any) {
+        
+//        selectedIndex = segmentedControl.selectedIndex
+//        print(selectedIndex)
+    }
+    
+    @objc func handleGesture(_ gesture: UISwipeGestureRecognizer) {
+        
+//        if gesture.direction == .left {
+//
+//            let index = segmentedControl.selectedIndex + 1
+//            if index <= SelectedState.card.rawValue {
+//                segmentedControl.selectedIndex = index
+//                selectedSegmentIndex = ReceiveState(rawValue: index)!
+//                setupButtons(selectedButton: activeButton, animation: true)
+//            }
+//        }
+//        else if gesture.direction == .right {
+//
+//            let index = selectedSegmentIndex.rawValue - 1
+//            if index >= 0 {
+//                selectedSegmentIndex = ReceiveState(rawValue: index)!
+//                setupButtons(selectedButton: activeButton, animation: true)
+//            }
+//        }
+    }
+}
+
+//MARK: - SetupUI
+private extension ReceiveContainerViewController {
+
+    func setupSwipeGestures() {
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture(_:)))
+        scrollViewContainer.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture(_:)))
+        swipeLeft.direction = .left
+        scrollViewContainer.addGestureRecognizer(swipeLeft)
+    }
+    
+    func setupSegmentedControl() {
         
         let buttons: [SegmentedControl.Button] = [.init(name: Localizable.Receive.Button.cryptocurrency,
                                                         icon: .init(normal: Images.rGateway14Basic500.image,
-                                                        selected: Images.rGateway14White.image)),
+                                                                    selected: Images.rGateway14White.image)),
                                                   
                                                   .init(name: Localizable.Receive.Button.invoice,
                                                         icon: .init(normal: Images.rInwaves14Basic500.image,
@@ -36,23 +96,6 @@ final class ReceiveContainerViewController: UIViewController {
         
         
         segmentedControl.update(with: buttons)
-        
-    }
-    
-  
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        setupBigNavigationBar()
-    }
-}
-
-//MARK: - Actions
-private extension ReceiveContainerViewController {
-    @IBAction func segmentedDidChange(_ sender: Any) {
-        
-        guard selectedIndex != segmentedControl.selectedIndex else { return }
-        selectedIndex = segmentedControl.selectedIndex
-        print(selectedIndex)
     }
 }
 
