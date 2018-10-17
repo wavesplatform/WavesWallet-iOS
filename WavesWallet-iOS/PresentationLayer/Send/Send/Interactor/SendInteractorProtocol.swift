63622d91a82f48432ccae068bf0700c874e83936
Pavel Gubin <xdozorx@gmail.com>
2018-10-17 04:30:22 +0300

    update

---
+++ WavesWallet-iOS/PresentationLayer/Send/Send/Interactor/SendInteractorProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  SendInteractorProtocol.swift
+//  WavesWallet-iOS
+//
+//  Created by Pavel Gubin on 10/17/18.
+//  Copyright © 2018 Waves Platform. All rights reserved.
+//
+
+import Foundation
+
+protocol SendInteractorProtocol {
+    
+}
