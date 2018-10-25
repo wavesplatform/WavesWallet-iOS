226ebbaab7a528be35540bcfcc50d8e6bdc72724
mefilt <mefilt@gmail.com>
2018-10-25 19:56:04 +0300

    WIP Profile

---
+++ WavesWallet-iOS/PresentationLayer/Profile/AddressesKeys/AddressesKeysViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddressesKeysViewController.swift
+//  WavesWallet-iOS
+//
+//  Created by mefilt on 25/10/2018.
+//  Copyright © 2018 Waves Platform. All rights reserved.
+//
+
+import UIKit
+
+final class AddressesKeysViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+x   
+}
