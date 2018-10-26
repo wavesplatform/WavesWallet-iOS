7aa012205594e1a7169e9ccdeac33b9d69408427
Pavel Gubin <xdozorx@gmail.com>
2018-10-26 13:21:07 +0300

    add browser to receive card

--- WavesWallet-iOS/PresentationLayer/Receive/Card/ReceiveCardViewController.swift
+++ WavesWallet-iOS/PresentationLayer/Receive/Card/ReceiveCardViewController.swift
@@ -53,10 +53,12 @@ final class ReceiveCardViewController: UIViewController {
 
     @IBAction private func continueTapped(_ sender: Any) {
     
-        debug(self.urlLink)
-        //TODO: - Need to show webView screen
-        let vc = StoryboardScene.Receive.receiveCardCompleteViewController.instantiate()
-        navigationController?.pushViewController(vc, animated: true)
+        let browser = BrowserViewController(url: URL(string: urlLink)!)
+        let nav = UINavigationController(rootViewController: browser)
+        present(nav, animated: true, completion: {
+            let vc = StoryboardScene.Receive.receiveCardCompleteViewController.instantiate()
+            self.navigationController?.pushViewController(vc, animated: false)
+        })
     }
     
     @IBAction private func changeCurrency(_ sender: Any) {
