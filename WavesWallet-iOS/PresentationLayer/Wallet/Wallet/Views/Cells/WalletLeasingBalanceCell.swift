89679661586571d3e05be0f985c73b74d04c9125
mefilt <mefilt@gmail.com>
2018-11-01 18:56:38 +0300

    Begin leasing

--- WavesWallet-iOS/PresentationLayer/Wallet/Wallet/Views/Cells/WalletLeasingBalanceCell.swift
+++ WavesWallet-iOS/PresentationLayer/Wallet/Wallet/Views/Cells/WalletLeasingBalanceCell.swift
@@ -12,6 +12,7 @@ fileprivate enum Constants {
     static let statusBarPadding: CGFloat = 32
     static let statusBarMinMediumPercent: CGFloat = 3.5
     static let statusBarMinSmallPercent: CGFloat = 2
+    static let height: CGFloat = 290
 }
 
 protocol WalletLeasingBalanceCellDelegate: AnyObject {
@@ -22,23 +23,18 @@ protocol WalletLeasingBalanceCellDelegate: AnyObject {
 final class WalletLeasingBalanceCell: UITableViewCell, Reusable {
     @IBOutlet var viewContainer: UIView!
 
-    @IBOutlet var totalBalanceTitleLabel: UILabel!
     @IBOutlet var avaliableTitleLabel: UILabel!
     @IBOutlet var leasedTitleLabel: UILabel!
-    @IBOutlet var leasedInTitleLabel: UILabel!
+    @IBOutlet var totalBalanceTitleLabel: UILabel!
 
-    @IBOutlet var labelBalance: UILabel!
     @IBOutlet var labelAvaliableBalance: UILabel!
     @IBOutlet var leasedBalanceLabel: UILabel!
-    @IBOutlet var leasedInBalanceLabel: UILabel!
+    @IBOutlet var labelTotalBalance: UILabel!
 
     @IBOutlet var buttonStartLease: UIButton!
     @IBOutlet var leasedWidth: NSLayoutConstraint!
-    @IBOutlet var leasedInWidth: NSLayoutConstraint!
-    @IBOutlet var viewLeasedInHeight: NSLayoutConstraint!
 
     private var leasedPercent: CGFloat = 0
-    private var leasedInPercent: CGFloat = 0
 
     private var availableMoney: Money!
     
@@ -55,13 +51,12 @@ final class WalletLeasingBalanceCell: UITableViewCell, Reusable {
 
         let viewWidth = frame.width - Constants.statusBarPadding * 2
         leasedWidth.constant = leasedPercent * viewWidth / 100
-        leasedInWidth.constant =  leasedInPercent * viewWidth / 100
 
         super.updateConstraints()
     }
 
     class func cellHeight() -> CGFloat {
-        return 326
+        return Constants.height
     }
 
     @objc func startLease() {
@@ -73,10 +68,9 @@ final class WalletLeasingBalanceCell: UITableViewCell, Reusable {
 
 extension WalletLeasingBalanceCell: Localization {
     func setupLocalization() {
-        totalBalanceTitleLabel.text = Localizable.Waves.Wallet.Label.totalBalance
         avaliableTitleLabel.text = Localizable.Waves.Wallet.Label.available
         leasedTitleLabel.text = Localizable.Waves.Wallet.Label.leased
-        leasedInTitleLabel.text = Localizable.Waves.Wallet.Label.leasedIn
+        totalBalanceTitleLabel.text = Localizable.Waves.Wallet.Label.totalBalance
         buttonStartLease.setTitle(Localizable.Waves.Wallet.Button.startLease, for: .normal)
     }
 }
@@ -89,37 +83,26 @@ extension WalletLeasingBalanceCell: ViewConfiguration {
         setupLocalization()
         availableMoney = model.avaliableMoney
         
-        labelBalance.attributedText = .styleForBalance(text: model.totalMoney.displayTextFull,
-                                                       font: labelAvaliableBalance.font)
         labelAvaliableBalance.attributedText = .styleForBalance(text: model.avaliableMoney.displayTextFull,
-                                                                font: labelAvaliableBalance.font)
-        leasedInBalanceLabel.attributedText = .styleForBalance(text: model.leasedInMoney.displayTextFull,
-                                                               font: leasedInBalanceLabel.font)
+                                                       font: labelAvaliableBalance.font)
+
         leasedBalanceLabel.attributedText = .styleForBalance(text: model.leasedMoney.displayTextFull,
-                                                             font: leasedBalanceLabel.font)
+                                                                font: leasedBalanceLabel.font)
+
+        labelTotalBalance.attributedText = .styleForBalance(text: model.totalMoney.displayTextFull,
+                                                             font: labelTotalBalance.font)
 
         leasedPercent = CGFloat(model.leasedMoney.amount) / CGFloat(model.avaliableMoney.amount) * 100
-        leasedInPercent = CGFloat(model.leasedInMoney.amount + model.leasedMoney.amount) / CGFloat(model.avaliableMoney.amount) * 100
 
         if leasedPercent < Constants.statusBarMinSmallPercent {
-            leasedPercent = leasedPercent > leasedInPercent ? Constants.statusBarMinMediumPercent : Constants.statusBarMinSmallPercent
+            leasedPercent = Constants.statusBarMinSmallPercent
         }
 
-        if leasedInPercent < Constants.statusBarMinSmallPercent {
-            let offSet = leasedPercent > leasedInPercent ? Constants.statusBarMinMediumPercent : Constants.statusBarMinSmallPercent
-            leasedInPercent = leasedPercent + offSet
-        }
-
-        if leasedInPercent.isNaN {
-            leasedInPercent = 0
-        }
-        
         if leasedPercent.isNaN {
             leasedPercent = 0
         }
 
         leasedPercent = min(leasedPercent, 100)
-        leasedInPercent = min(leasedInPercent, 100)
 
         setNeedsUpdateConstraints()
     }
