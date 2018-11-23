f15984786b7e519566ca7a549a20c0f00258fd42
ermolenkoj <elliottheproducer@gmail.com>
2018-11-23 17:27:20 +0300

    fixes

--- WavesWallet-iOS/PresentationLayer/Modules/Hello/InfoPages/View/ShortInfoPageView.swift
+++ WavesWallet-iOS/PresentationLayer/Modules/Hello/InfoPages/View/ShortInfoPageView.swift
@@ -7,27 +7,50 @@
 
 import UIKit
 
+protocol ShortInfoPageViewDelegate: class {
+    
+    func shortInfoPageViewDidScrollToBottom(view: ShortInfoPageView)
+    
+}
+
 final class ShortInfoPageView: UIView {
 
-    struct Model {
+    class Model {
         let title: String
         let detail: String
         let firstImage: UIImage?
         let secondImage: UIImage?
         let thirdImage: UIImage?
         let fourthImage: UIImage?
+        var scrolledToBottom: Bool = false
+        
+        init(title: String, detail: String, firstImage: UIImage?, secondImage: UIImage?, thirdImage: UIImage?, fourthImage: UIImage?) {
+            self.title = title
+            self.detail = detail
+            self.firstImage = firstImage
+            self.secondImage = secondImage
+            self.thirdImage = thirdImage
+            self.fourthImage = fourthImage
+        }
     }
+    
+    weak var delegate: ShortInfoPageViewDelegate?
     
     @IBOutlet private weak var titleTopConstraint: NSLayoutConstraint!
     @IBOutlet private weak var titleLeadingConstraint: NSLayoutConstraint!
     @IBOutlet private weak var titleTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
+    
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var detailLabel: UILabel!
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var firstImageView: UIImageView!
     @IBOutlet weak var secondImageView: UIImageView!
     @IBOutlet weak var thirdImageView: UIImageView!
     @IBOutlet weak var fourthImageView: UIImageView!
+    
+    @IBOutlet weak var contentView: UIView!
     
     func setupConstraints() {
         if Platform.isIphone5 {
@@ -44,6 +67,35 @@ final class ShortInfoPageView: UIView {
         titleLeadingConstraint.constant = titleLeading
         titleTrailingConstraint.constant = titleTrailing
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+   
+//        let contentHeight = String(describing: contentView.bounds.height)
+//        print("LAYOUT CONTENT HEIGHT: " + contentHeight)
+//        updateOnScroll()
+    }
+//
+//    override func updateConstraints() {
+//        super.updateConstraints()
+//
+//        let contentHeight = String(describing: scrollView.contentSize.height)
+//        let height = String(describing: scrollView.frame.height)
+////        print("UPDATE CONTENT HEIGHT: " + contentHeight + " " + height)
+//    }
+    
+    func updateOnScroll() {
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.height
+        let contentOffsetY = scrollView.contentOffset.y
+        
+        print(contentHeight, contentHeight - height - contentOffsetY)
+        if contentHeight > 0 && contentHeight - height - contentOffsetY <= imageViewBottomConstraint.constant {
+            delegate?.shortInfoPageViewDidScrollToBottom(view: self)
+        }
+        
+    }
+    
 }
 
 extension ShortInfoPageView: ViewConfiguration {
@@ -59,7 +111,19 @@ extension ShortInfoPageView: ViewConfiguration {
         fourthImageView.image = model.fourthImage
         
         setupConstraints()
+        scrollView.delegate = self
         
+        setNeedsLayout()
+        layoutIfNeeded()
+        updateOnScroll()
+    }
+    
+}
+
+extension ShortInfoPageView: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        updateOnScroll()
     }
     
 }
