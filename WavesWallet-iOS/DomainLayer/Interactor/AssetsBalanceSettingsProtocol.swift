42e8d3cb38215b2588494ac825af9253d3f9254c
mefilt <mefilt@gmail.com>
2018-12-04 20:36:47 +0300

    WIP Added Asset

--- WavesWallet-iOS/DomainLayer/Interactor/AssetsBalanceSettingsProtocol.swift
+++ WavesWallet-iOS/DomainLayer/Interactor/AssetsBalanceSettingsProtocol.swift
@@ -11,23 +11,67 @@ import RxSwift
 
 protocol AssetsBalanceSettingsInteractorProtocol {
 
-    func settings(by ids: [String]) -> Observable<[DomainLayer.DTO.AssetBalanceSettings]>
+    func settings(by accountAddress: String, ids: [String]) -> Observable<[DomainLayer.DTO.AssetBalanceSettings]>
 }
 
 final class AssetsBalanceSettingsInteractor: AssetsBalanceSettingsInteractorProtocol {
 
     private let assetsBalanceSettingsRepository: AssetsBalanceSettingsRepositoryProtocol = FactoryRepositories.instance.assetsBalanceSettingsRepositoryLocal
 
-    func settings(by ids: [String]) -> Observable<[DomainLayer.DTO.AssetBalanceSettings]> {
+    func settings(by accountAddress: String, ids: [String]) -> Observable<[DomainLayer.DTO.AssetBalanceSettings]> {
 
-        let settings = ids.map { (id) -> DomainLayer.DTO.AssetBalanceSettings in
-            return DomainLayer.DTO.AssetBalanceSettings(assetId: id,
-                                                        sortLevel: 1,
-                                                        isHidden: false,
-                                                        isFavorite: false)
+        let enviroments = Environments.current
+
+        let settings = assetsBalanceSettingsRepository
+            .settings(by: accountAddress, ids: ids)
+            .flatMapLatest { (mapSettings) -> Observable<[DomainLayer.DTO.AssetBalanceSettings]> in
+
+                let sortedSettings = mapSettings
+                    .reduce(into: [DomainLayer.DTO.AssetBalanceSettings](), { $0.append($1.value) })
+                    .sorted(by: { $0.sortLevel > $1.sortLevel })
+
+                let first = sortedSettings.first
+                let last = sortedSettings.last
+
+
+                return Observable.never()
+            }
+
+
+
+
+//        let settings = ids.map { (id) -> DomainLayer.DTO.AssetBalanceSettings in
+//            return DomainLayer.DTO.AssetBalanceSettings(assetId: id,
+//                                                        sortLevel: 1,
+//                                                        isHidden: false,
+//                                                        isFavorite: false)
+//        }
+//
+
+        return Observable.just([])
+    }
+
+    private func createSetting(by assetId: String,
+                               first: DomainLayer.DTO.AssetBalanceSettings?,
+                               last: DomainLayer.DTO.AssetBalanceSettings?,
+                               mapSettings: [String: DomainLayer.DTO.AssetBalanceSettings],
+                               enviroments: Environment) -> DomainLayer.DTO.AssetBalanceSettings? {
+
+        let enviromentAsset = enviroments.generalAssetIds.enumerated().first(where: { $0.element.assetId == assetId })
+
+        if let enviromentAsset = enviromentAsset {
+            let prevIndex =  (enviromentAsset.offset - 1)
+            let enviromentAssetPrev = prevIndex >= 0 ? enviroments.generalAssetIds[prevIndex] : nil
+
+
+        } else {
+
         }
 
 
-        return Observable.just(settings)
+
+
+
+        return nil
     }
 }
