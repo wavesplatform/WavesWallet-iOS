7d635979b4b2308323429ce210ecbb708539bdab
mefilt <mefilt@gmail.com>
2018-12-14 15:03:48 +0300

    WIP

--- WavesWallet-iOS/PresentationLayer/Modules/Support/TestSVGViewController.swift
+++ WavesWallet-iOS/PresentationLayer/Modules/Support/TestSVGViewController.swift
@@ -15,6 +15,7 @@ final class TestSVGViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     var models: [String] = ["https://raw.githubusercontent.com/wavesplatform/WavesWallet-iOS/feature/IOS-418/b-logos/Bettertokens.svg",
                             "https://raw.githubusercontent.com/wavesplatform/WavesWallet-iOS/feature/IOS-418/b-logos/manhammock.svg",
+                            "https://raw.githubusercontent.com/wavesplatform/WavesWallet-iOS/feature/IOS-418/b-logos/Waves.svg",
                             "https://raw.githubusercontent.com/wavesplatform/WavesWallet-iOS/feature/IOS-418/b-logos/Waves.svg"]
 }
 
