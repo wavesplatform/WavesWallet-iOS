4040fe4082416416b4ce57452fdae316a7867d43
Pavel Gubin <xdozorx@gmail.com>
2018-12-19 15:23:55 +0200

    Fix IOS-405

--- WavesWallet-iOS/PresentationLayer/Modules/Send/Send/SendViewController.swift
+++ WavesWallet-iOS/PresentationLayer/Modules/Send/Send/SendViewController.swift
@@ -30,7 +30,6 @@ final class SendViewController: UIViewController {
     @IBOutlet private weak var labelWarningTitle: UILabel!
     @IBOutlet private weak var labelWarningSubtitle: UILabel!
     @IBOutlet private weak var labelWarningDescription: UILabel!
-    @IBOutlet private weak var labelWarningDescription2: UILabel!    
     @IBOutlet private weak var amountView: AmountInputView!
     @IBOutlet private weak var recipientAddressView: AddressInputView!
     @IBOutlet private weak var buttonContinue: HighlightedButton!
@@ -507,8 +506,32 @@ private extension SendViewController {
         let max = info.maxAmount.displayText + " " + info.assetShortName
 
         labelWarningSubtitle.text = Localizable.Waves.Send.Label.Warning.subtitle(info.assetName, min, max)
-        labelWarningDescription.text = Localizable.Waves.Send.Label.Warning.description(info.assetShortName, info.assetName)
-        labelWarningDescription2.text = Localizable.Waves.Send.Label.Warning.description2(info.assetShortName)
+        
+        if selectedAsset?.asset.isEthereum == true {
+            let smartcontractsTitle = Localizable.Waves.Send.Label.Warning.Descriptionsmartcontracts.title(info.assetShortName, info.assetName)
+            let smartcontractsSubtitle = Localizable.Waves.Send.Label.Warning.Descriptionsmartcontracts.subtitle(info.assetShortName)
+            let spaces = "\n\n"
+            let text = smartcontractsTitle + spaces + smartcontractsSubtitle
+            let attrParams = [NSAttributedString.Key.font : UIFont.systemFont(ofSize: labelWarningDescription.font.pointSize, weight: .semibold)]
+
+            let attr = NSMutableAttributedString(string: text, attributes: attrParams)
+            
+            attr.addAttribute(NSAttributedString.Key.font,
+                              value: UIFont.systemFont(ofSize: labelWarningDescription.font.pointSize),
+                              range: (text as NSString).range(of: smartcontractsSubtitle))
+            
+            attr.addAttribute(NSAttributedString.Key.font,
+                              value: UIFont.systemFont(ofSize: 5),
+                              range: (text as NSString).range(of: spaces))
+            
+            labelWarningDescription.attributedText = attr
+        }
+        else {
+            let attrParams = [NSAttributedString.Key.font : UIFont.systemFont(ofSize: labelWarningDescription.font.pointSize)]
+            let attr = NSAttributedString(string: Localizable.Waves.Send.Label.Warning.description(info.assetName),
+                                          attributes: attrParams)
+            labelWarningDescription.attributedText = attr
+        }
         
         viewWarning.isHidden = false
         viewWarning.alpha = 0
