d842ecd390623e5d16b7fa62b2816a5b65b15540
Pavel Gubin <xdozorx@gmail.com>
2018-12-23 19:06:02 +0200

    remove NetworkManager from ReceiveCardInteractor

--- WavesWallet-iOS/DomainLayer/Repositories/CoinomatRepositoryProtocol.swift
+++ WavesWallet-iOS/DomainLayer/Repositories/CoinomatRepositoryProtocol.swift
@@ -13,4 +13,5 @@ protocol CoinomatRepositoryProtocol {
     func tunnelInfo(currencyFrom: String, currencyTo: String, walletTo: String, moneroPaymentID: String?) -> Observable<DomainLayer.DTO.Coinomat.TunnelInfo>
     func getRate(asset: DomainLayer.DTO.Asset) -> Observable<DomainLayer.DTO.Coinomat.Rate>
     func cardLimits(address: String, fiat: String) -> Observable<DomainLayer.DTO.Coinomat.CardLimit>
+    func getPrice(address: String, amount: Money, type: ReceiveCard.DTO.FiatType) -> Observable<Money>
 }
