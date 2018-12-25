b943e280a0ea0ebc6af0ab6b2fbbddbaa14aeedc
mefilt <mefilt@gmail.com>
2018-12-25 19:16:05 +0300

    WIP

---
+++ WavesWallet-iOS/PresentationLayer/Coordinators/Passcode/PasscodeChangePasscodeCoordinator.swift
@@ -0,0 +1,67 @@
+//
+//  PasscodeChangePasscodeCoordinator.swift
+//  WavesWallet-iOS
+//
+//  Created by mefilt on 25/12/2018.
+//  Copyright © 2018 Waves Platform. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol PasscodeChangePasscodeCoordinatorDelegate: AnyObject {
+    func passcodeCoordinatorAuthorizationCompleted(wallet: DomainLayer.DTO.Wallet)
+}
+
+final class PasscodeChangePasscodeCoordinator: Coordinator {
+
+    var childCoordinators: [Coordinator] = []
+    weak var parent: Coordinator?
+
+    private let navigationRouter: NavigationRouter
+    private let password: String
+    private let wallet: DomainLayer.DTO.Wallet
+
+    weak var delegate: PasscodeCoordinatorDelegate?
+
+    init(navigationRouter: NavigationRouter, wallet: DomainLayer.DTO.Wallet, password: String) {
+
+        self.navigationRouter = navigationRouter
+        self.wallet = wallet
+        self.password = password
+    }
+
+    func start() {
+
+        let vc = PasscodeModuleBuilder(output: self)
+            .build(input: .init(kind: .changePasscodeByPassword(wallet, password: password),
+                                hasBackButton: true))
+
+        navigationRouter.pushViewController(vc, animated: true) { [weak self] in
+            self?.removeFromParentCoordinator()
+        }
+    }
+
+    private func dissmiss() {
+        navigationRouter.popViewController()
+        removeFromParentCoordinator()
+    }
+}
+
+// MARK: PasscodeOutput
+extension PasscodeChangePasscodeCoordinator: PasscodeModuleOutput {
+
+    func passcodeVerifyAccessCompleted(_ wallet: DomainLayer.DTO.SignedWallet) {
+        delegate?.passcodeCoordinatorVerifyAcccesCompleted(signedWallet: wallet)
+    }
+
+    func passcodeTapBackButton() {
+        dissmiss()
+    }
+
+    func passcodeLogInCompleted(passcode: String, wallet: DomainLayer.DTO.Wallet, isNewWallet: Bool) {}
+
+    func passcodeUserLogouted() {}
+
+    func passcodeLogInByPassword() {}
+}
