e924f609bde20eabebf8e0c62e2b21614aa23056
Pavel Gubin <xdozorx@gmail.com>
2019-01-14 17:37:30 +0200

    update DexOrderBookRepositoryProtocol

---
+++ WavesWallet-iOS/DomainLayer/Queries/DomainDexQueries.swift
@@ -0,0 +1,35 @@
+//
+//  OrderMatcherQueries.swift
+//  WavesWallet-iOS
+//
+//  Created by Pavel Gubin on 12/26/18.
+//  Copyright © 2018 Waves Platform. All rights reserved.
+//
+
+import Foundation
+
+extension DomainLayer.Query {
+    
+    enum Dex {
+        
+        struct CreateOrder {
+            let wallet: DomainLayer.DTO.SignedWallet
+            let matcherPublicKey: PublicKeyAccount
+            let amountAsset: String
+            let priceAsset: String
+            let amount: Int64
+            let price: Int64
+            let orderType: DomainLayer.DTO.Dex.OrderType
+            let matcherFee: Int
+            let timestamp: Int64 = Date().millisecondsSince1970
+            let expiration: Int
+        }
+        
+        struct CancelOrder {
+            let wallet: DomainLayer.DTO.SignedWallet
+            let orderId: String
+            let amountAsset: String
+            let priceAsset: String
+        }
+    }
+}
