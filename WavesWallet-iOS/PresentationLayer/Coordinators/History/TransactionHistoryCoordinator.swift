bf6e58d5cbcfedb9bd4a95e6aeecabf8fca3a202
Pavel Gubin <xdozorx@gmail.com>
2019-01-25 01:54:25 +0200

    show fee error at cancel leasing

--- WavesWallet-iOS/PresentationLayer/Coordinators/History/TransactionHistoryCoordinator.swift
+++ WavesWallet-iOS/PresentationLayer/Coordinators/History/TransactionHistoryCoordinator.swift
@@ -129,7 +129,7 @@ extension TransactionHistoryCoordinator: AddAddressBookModuleOutput {
             
             let cancelOrder = StartLeasingTypes.DTO.CancelOrder(leasingTX: transaction.id,
                                                                amount: leasing.balance.money,
-                                                               fee: transaction.totalFee.money)
+                                                               fee: Money(0, 0))
             let vc = StartLeasingConfirmModuleBuilder(output: self, errorDelegate: nil).build(input: .cancel(cancelOrder))
             router.pushViewController(vc, animated: true)
 
