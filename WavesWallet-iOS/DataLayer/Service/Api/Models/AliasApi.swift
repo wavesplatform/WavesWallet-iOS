d34f8b315263820c888a89973cb9aea18c59693f
Pavel Gubin <xdozorx@gmail.com>
2019-01-30 10:56:49 +0200

    fix IOS-493

---
+++ WavesWallet-iOS/DataLayer/Service/Api/Models/AliasApi.swift
@@ -0,0 +1,17 @@
+//
+//  AliasApi.swift
+//  WavesWallet-iOS
+//
+//  Created by Pavel Gubin on 1/30/19.
+//  Copyright © 2019 Waves Platform. All rights reserved.
+//
+
+import Foundation
+
+extension API.DTO {
+    
+    struct Alias: Decodable {
+        let alias: String
+        let address: String?
+    }
+}
