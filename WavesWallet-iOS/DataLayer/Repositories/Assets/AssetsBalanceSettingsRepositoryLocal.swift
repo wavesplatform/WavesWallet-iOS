85dfb56dbd02ddd1db0d94c31c739c710a9d0979
mefilt <mefilt@gmail.com>
2019-02-18 16:58:18 +0300

    IOS-502 Refactor

--- WavesWallet-iOS/DataLayer/Repositories/Assets/AssetsBalanceSettingsRepositoryLocal.swift
+++ WavesWallet-iOS/DataLayer/Repositories/Assets/AssetsBalanceSettingsRepositoryLocal.swift
@@ -11,25 +11,6 @@ import RxSwift
 import RealmSwift
 import RxRealm
 
-enum RepositoryError: Error {
-    case fail
-    case notFound
-}
-
-extension Float {
-    var notFound: Float {
-        return -1
-    }
-}
-
-protocol AssetsBalanceSettingsRepositoryProtocol {
-    func settings(by accountAddress: String, ids: [String]) -> Observable<[String: DomainLayer.DTO.AssetBalanceSettings]>
-    func settings(by accountAddress: String) -> Observable<[DomainLayer.DTO.AssetBalanceSettings]>
-    func listenerSettings(by accountAddress: String, ids: [String]) -> Observable<[DomainLayer.DTO.AssetBalanceSettings]>
-    func saveSettings(by accountAddress: String, settings: [DomainLayer.DTO.AssetBalanceSettings]) -> Observable<Bool>
-    func removeBalancesSettting(actualIds: [String], accountAddress: String) -> Observable<Bool>
-}
-
 final class AssetsBalanceSettingsRepositoryLocal: AssetsBalanceSettingsRepositoryProtocol {
 
     func removeBalancesSettting(actualIds: [String], accountAddress: String) -> Observable<Bool> {
