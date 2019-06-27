980b3ca1becb867a1f055aa8f94ff6ec984ec88e
Pavel Gubin <xdozorx@gmail.com>
2019-06-27 10:55:19 +0300

    update

--- DataLayer/Sources/Repositories/GatewayRepository.swift
+++ DataLayer/Sources/Repositories/GatewayRepository.swift
@@ -9,17 +9,30 @@
 import Foundation
 import RxSwift
 import DomainLayer
+import Moya
+import Extensions
 
 final class GatewayRepository: GatewayRepositoryProtocol {    
     
-    private let environmentRepository: EnvironmentRepositoryProtocols
+    private let gatewayProvider: MoyaProvider<Gateway.Service> = .anyMoyaProvider()
     
-    init(environmentRepository: EnvironmentRepositoryProtocols) {
-        self.environmentRepository = environmentRepository
-    }
-    
-    func initWithdrawProcess(by address: String, assetId: String) -> Observable<Bool> {
-        return Observable.empty()
+    func initWithdrawProcess(by address: String, asset: DomainLayer.DTO.Asset, accountAddress: String) -> Observable<DomainLayer.DTO.Gateway.InitWithdrawProcess> {
+        
+        let initWithdraw = Gateway.Service.InitProcess(userAddress: address, assetId: asset.id)
+        return gatewayProvider.rx
+            .request(.initProcess(initWithdraw), callbackQueue:  DispatchQueue.global(qos: .userInteractive))
+            .filterSuccessfulStatusAndRedirectCodes()
+            .map(Gateway.DTO.Withdraw.self)
+            .asObservable()
+            .map({ (initWithdraw) -> DomainLayer.DTO.Gateway.InitWithdrawProcess in
+                return DomainLayer.DTO.Gateway.InitWithdrawProcess(
+                    recipientAddress: initWithdraw.recipientAddress,
+                    minAmount: Money(initWithdraw.minAmount, asset.precision),
+                    maxAmount:  Money(initWithdraw.maxAmount, asset.precision),
+                    fee:  Money(initWithdraw.fee, asset.precision),
+                    processId: initWithdraw.processId)
+                
+            })
     }
     
     func sendWithdraw() -> Observable<Bool> {
