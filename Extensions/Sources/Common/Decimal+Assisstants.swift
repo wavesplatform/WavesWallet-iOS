45cabe80c563686f8c0d078561c0d66ff67dd2cf
Mefilt <Mefilt@gmail.com>
2019-08-08 22:41:47 +0300

    WIP

--- Extensions/Sources/Common/Decimal+Assisstants.swift
+++ Extensions/Sources/Common/Decimal+Assisstants.swift
@@ -9,42 +9,7 @@
 import Foundation
 
 public extension Decimal {
-    
-    //We ovveride this methods because is Foundation framework Decimal has a bug when it init from (U)Int64
-    //  https://github.com/apple/swift/commit/2f4e70bf7f4eee43bfb2f24d6215eb1f63c05d01
-    
-    //TODO: can be fixed with new xCode 10.2, need to check
-    //TODO: Exeperement
-//    public init(_ value: UInt64) {
-//        self = Decimal()
-//        if value == 0 {
-//            return
-//        }
-//
-//        var compactValue = value
-//        var exponent: Int32 = 0
-//        while compactValue % 10 == 0 {
-//            compactValue = compactValue / 10
-//            exponent = exponent + 1
-//        }
-//        _isCompact = 1
-//        _exponent = exponent
-//
-//        let wordCount = ((UInt64.bitWidth - compactValue.leadingZeroBitCount) + (UInt16.bitWidth - 1)) / UInt16.bitWidth
-//        _length = UInt32(wordCount)
-//        _mantissa.0 = UInt16(truncatingIfNeeded: compactValue >> 0)
-//        _mantissa.1 = UInt16(truncatingIfNeeded: compactValue >> 16)
-//        _mantissa.2 = UInt16(truncatingIfNeeded: compactValue >> 32)
-//        _mantissa.3 = UInt16(truncatingIfNeeded: compactValue >> 48)
-//    }
-//
-//    public init(_ value: Int64) {
-//        self.init(value.magnitude)
-//        if value < 0 {
-//            _isNegative = 1
-//        }
-//    }
-    
+        
     func rounded() -> Decimal {
         
         let behavior = NSDecimalNumberHandler(roundingMode: .down,
