22ffb5a766f3fb860bcacd0c7e3999aa6d915012
Mefilt <Mefilt@gmail.com>
2019-08-15 16:10:58 +0300

    WIP

---
+++ Extensions/Sources/Common/DeepLink.swift
@@ -0,0 +1,13 @@
+//
+//  DeepLink\.swift
+//  Extensions
+//
+//  Created by rprokofev on 15.08.2019.
+//  Copyright © 2019 Waves Platform. All rights reserved.
+//
+
+import Foundation
+
+struct DeepLink {
+    
+}
