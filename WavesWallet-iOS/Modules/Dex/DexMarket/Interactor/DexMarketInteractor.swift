c4c6a5f7cde5a0cc2517933039ddf76d172722e4
Pavel <xdozorx@gmail.com>
2019-09-13 16:15:02 +0300

    IOS-802

--- WavesWallet-iOS/Modules/Dex/DexMarket/Interactor/DexMarketInteractor.swift
+++ WavesWallet-iOS/Modules/Dex/DexMarket/Interactor/DexMarketInteractor.swift
@@ -3,9 +3,14 @@ import RxSwift
 import DomainLayer
 import DataLayer
 import Extensions
+import WavesSDK
 
 private enum Constants {
     static let firstGeneralAssets = 4
+    
+    static let liquidToken = "7FzrHF1pueRFrPEupz6oiVGTUZqe8epvC7ggWUx8n1bd"
+    static let wctToken = "DHgwrRvVyqJsepd32YbBqUeDH4GJ1N984X8QoekjgH8J"
+    static let mrtToken = "4uK8i4ThRGbehENwa6MxyLtxAjAo1Rj9fduborGExarC"
 }
 
 final class DexMarketInteractor: DexMarketInteractorProtocol {
@@ -21,8 +26,10 @@ final class DexMarketInteractor: DexMarketInteractorProtocol {
     private let environment = UseCasesFactory.instance.repositories.environmentRepository
     private let orderBookRepository = UseCasesFactory.instance.repositories.dexOrderBookRepository
 
-    private let dexPairsPriceRepository = UseCasesFactory.instance.repositories.dexPairsPriceRepository
-    private let assetsInteractor = UseCasesFactory.instance.assets
+    private let dexPairsPriceRepository: DexPairsPriceRepositoryProtocol = UseCasesFactory.instance.repositories.dexPairsPriceRepository
+    private let assetsInteractor: AssetsUseCaseProtocol = UseCasesFactory.instance.assets
+    private let assetsRepository: AssetsRepositoryProtocol = UseCasesFactory.instance.repositories.assetsRepositoryRemote
+    private let correctionPairsUseCase: CorrectionPairsUseCaseProtocol = UseCasesFactory.instance.correctionPairsUseCase
     
     func pairs() -> Observable<[DomainLayer.DTO.Dex.SmartPair]> {
 
@@ -39,75 +46,38 @@ final class DexMarketInteractor: DexMarketInteractorProtocol {
     }
     
     func searchPairs(searchWord: String) -> Observable<[DomainLayer.DTO.Dex.SmartPair]> {
-        return auth.authorizedWallet().flatMap({ [weak self] (wallet) -> Observable<[DomainLayer.DTO.Dex.SmartPair]>  in
-            guard let self = self else { return Observable.empty() }
-            return self.dexPairsPriceRepository.search(by: wallet.address, searchText: searchWord)
-                .flatMap({ [weak self] (pairs) -> Observable<[DomainLayer.DTO.Dex.SmartPair]> in
-                    
-                    guard let self = self else { return Observable.empty() }
-                    
-                    var assetsIds: [String] = []
-                    for pair in pairs {
-                        if !assetsIds.contains(pair.amountAsset) {
-                            assetsIds.append(pair.amountAsset)
-                        }
-                        
-                        if !assetsIds.contains(pair.priceAsset) {
-                            assetsIds.append(pair.priceAsset)
-                        }
-                    }
-                    
-                    return self.assetsInteractor.assets(by: assetsIds, accountAddress: wallet.address)
-                        .flatMap({ (assets) -> Observable<[DomainLayer.DTO.Dex.SmartPair]> in
-                            
-                            var dexPairs: [DomainLayer.DTO.Dex.Pair] = []
-                            
-                            for pair in pairs {
-                                if let amountAsset = assets.first(where: {$0.id == pair.amountAsset}),
-                                    let priceAsset = assets.first(where: {$0.id == pair.priceAsset}) {
-                                    
-                                    let dexAmountAsset = DomainLayer.DTO.Dex.Asset(id: amountAsset.id,
-                                                                                   name: amountAsset.displayName,
-                                                                                   shortName: amountAsset.ticker ?? amountAsset.displayName,
-                                                                                   decimals: amountAsset.precision)
-                                    
-                                    let dexPriceAsset = DomainLayer.DTO.Dex.Asset(id: priceAsset.id,
-                                                                                  name: priceAsset.displayName,
-                                                                                  shortName: priceAsset.ticker ?? priceAsset.displayName,
-                                                                                  decimals: priceAsset.precision)
-                                    dexPairs.append(.init(amountAsset: dexAmountAsset, priceAsset: dexPriceAsset))
-                                }
-                            }
-                            
-                            return self.dexRealmRepository.list(by: wallet.address)
-                                .map({ (localSmartPairs) -> [DomainLayer.DTO.Dex.SmartPair] in
-                                    
-                                    var newSmartPairs: [DomainLayer.DTO.Dex.SmartPair] = []
-                                    
-                                    for pair in dexPairs {
-                                        
-                                        let localPair = localSmartPairs.first(where: {$0.amountAsset.id == pair.amountAsset.id &&
-                                            $0.priceAsset.id == pair.priceAsset.id})
-                                        
-                                        let isGeneralAmount = assets.first(where: {$0.id == pair.amountAsset.id})?.isGeneral ?? false
-                                        let isGeneralPrice = assets.first(where: {$0.id == pair.priceAsset.id})?.isGeneral ?? false
-                                        let isGeneral = isGeneralAmount && isGeneralPrice
+        
+        let words = searchWord.components(separatedBy: "/").filter {$0.count > 0}
+            .map{$0.trimmingCharacters(in: .whitespaces)}
 
-                                        newSmartPairs.append(.init(amountAsset: pair.amountAsset,
-                                                                   priceAsset: pair.priceAsset,
-                                                                   isChecked: localPair?.isChecked ?? false,
-                                                                   isGeneral: isGeneral,
-                                                                   sortLevel: localPair?.sortLevel ?? 0))
-                                    }
-                                    
-                                    return newSmartPairs
-                                })
-                        })
-                })
-        })
-        .catchError({ _ -> Observable<[DomainLayer.DTO.Dex.SmartPair]> in
-            return Observable.just([])
-        })
+        return Observable.zip(auth.authorizedWallet(), environment.walletEnvironment())
+            .flatMap{ [weak self] (wallet, environment) -> Observable<[DomainLayer.DTO.Dex.SmartPair]> in
+                guard let self = self else { return Observable.empty() }
+                
+                if words.count == 1 {
+                
+                    var generalIds = environment.generalAssets.map { $0.assetId }
+                    generalIds.append(Constants.wctToken)
+                    generalIds.append(Constants.mrtToken)
+                    generalIds.append(Constants.liquidToken)
+
+                    let generalAssetsObserver = self.assetsInteractor.assets(by: generalIds, accountAddress: wallet.address)
+                    let assetsObserver = self.assetsRepository.searchAssets(search: words[0])
+                  
+                    return self.searchPairs(firstSearchAssets: assetsObserver, secondSearchAssets: generalAssetsObserver, address: wallet.address)
+                }
+                else if words.count > 1 {
+                    let firstAssetsObserver = self.assetsRepository.searchAssets(search: words[0])
+                    let secondsAssetsObserver = self.assetsRepository.searchAssets(search: words[1])
+
+                    return self.searchPairs(firstSearchAssets: firstAssetsObserver, secondSearchAssets: secondsAssetsObserver, address: wallet.address)
+                }
+                return Observable.just([])
+            }
+            .catchError({ (error) -> Observable<[DomainLayer.DTO.Dex.SmartPair]> in
+                return Observable.just([])
+            })
+
     }
     
     func checkMark(pair: DomainLayer.DTO.Dex.SmartPair) {
@@ -130,6 +100,91 @@ final class DexMarketInteractor: DexMarketInteractorProtocol {
 
 //MARK: - Load data
 private extension DexMarketInteractor {
+    
+    func searchPairs(firstSearchAssets: Observable<[DomainLayer.DTO.Asset]>, secondSearchAssets: Observable<[DomainLayer.DTO.Asset]>, address: String) -> Observable<[DomainLayer.DTO.Dex.SmartPair]> {
+    
+        return Observable.zip(firstSearchAssets, secondSearchAssets)
+            .flatMap{ [weak self] (firstAssets, secondAssets) -> Observable<[DomainLayer.DTO.Dex.SmartPair]> in
+                guard let self = self else { return Observable.empty() }
+                
+                return self.correctionAndSearchPairs(firstAssets: firstAssets.filter {$0.isSpam == false},
+                                                     secondAssets: secondAssets.filter {$0.isSpam == false},
+                                                     address: address)
+            }
+    }
+    
+    func correctionAndSearchPairs(firstAssets: [DomainLayer.DTO.Asset], secondAssets: [DomainLayer.DTO.Asset], address: String) -> Observable<[DomainLayer.DTO.Dex.SmartPair]> {
+        
+        var pairs: [DomainLayer.DTO.CorrectionPairs.Pair] = []
+        let allAssets = firstAssets + secondAssets
+        
+        for asset in firstAssets {
+            for secondAsset in secondAssets {
+                if asset.id != secondAsset.id {
+                    pairs.append(.init(amountAsset: asset.id, priceAsset: secondAsset.id))
+                }
+            }
+        }
+        
+        return self.correctionPairsUseCase.correction(pairs: pairs)
+            .flatMap{ [weak self] correctionPairs -> Observable<[DomainLayer.DTO.Dex.SmartPair]> in
+                guard let self = self else { return Observable.empty() }
+                
+                let searchQueryPairs = correctionPairs.map{ DomainLayer.Query.Dex.SearchPairs.Pair(amountAsset: $0.amountAsset,
+                                                                                                   priceAsset: $0.priceAsset) }
+                
+                let searchPairsObserver = self.dexPairsPriceRepository.searchPairs(.init(kind: .pairs(searchQueryPairs)))
+                let localPairsObserver = self.dexRealmRepository.list(by: address)
+                
+                return Observable.zip(searchPairsObserver, localPairsObserver)
+                    .map{ pairsSearch, localPairs -> [DomainLayer.DTO.Dex.SmartPair] in
+                        
+                        struct SmartVolumePair {
+                            let smartPair: DomainLayer.DTO.Dex.SmartPair
+                            let volume: Double
+                        }
+                        var smartPairs: [SmartVolumePair] = []
+                        
+                        for (index, queryPair) in searchQueryPairs.enumerated() {
+                            
+                            guard let amountAsset = allAssets.first(where: {$0.id == queryPair.amountAsset}) else { break }
+                            guard let priceAsset = allAssets.first(where: {$0.id == queryPair.priceAsset}) else { break }
+                           
+                            
+                            let volume = pairsSearch.pairs[index]?.volumeWaves ?? 0
+                            let localPair = localPairs.first(where: {$0.amountAsset.id == amountAsset.id &&
+                                $0.priceAsset.id == priceAsset.id})
+                            
+                            let isGeneralAmount = allAssets.first(where: {$0.id == amountAsset.id})?.isGeneral ?? false
+                            let isGeneralPrice = allAssets.first(where: {$0.id == priceAsset.id})?.isGeneral ?? false
+                            let isGeneral = isGeneralAmount && isGeneralPrice
+                            
+                            let dexAmountAsset = DomainLayer.DTO.Dex.Asset(id: amountAsset.id,
+                                                                           name:  amountAsset.displayName,
+                                                                           shortName: amountAsset.ticker ?? amountAsset.displayName,
+                                                                           decimals: amountAsset.precision)
+                            
+                            let dexPriceAsset = DomainLayer.DTO.Dex.Asset(id: priceAsset.id,
+                                                                          name: priceAsset.displayName,
+                                                                          shortName: priceAsset.ticker ?? priceAsset.displayName,
+                                                                          decimals: priceAsset.precision)
+                            
+                            smartPairs.append(.init(smartPair: .init(amountAsset: dexAmountAsset,
+                                                                     priceAsset: dexPriceAsset,
+                                                                     isChecked: localPair?.isChecked ?? false,
+                                                                     isGeneral: isGeneral,
+                                                                     sortLevel: localPair?.sortLevel ?? 0),
+                                                    volume: volume))
+
+                        }
+                        
+                      
+                        smartPairs.sort(by: {$0.volume > $1.volume})
+                        
+                        return smartPairs.map {$0.smartPair}
+                }
+        }
+    }
     
     func defaultPairs(wallet: DomainLayer.DTO.SignedWallet, environment: WalletEnvironment) -> Observable<[DomainLayer.DTO.Dex.SmartPair]> {
         
