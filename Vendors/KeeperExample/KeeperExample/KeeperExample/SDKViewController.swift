175c1857725c550e389526f331ebf7bbc021b95b
Pavel <xdozorx@gmail.com>
2019-09-18 01:03:38 +0300

    update

--- Vendors/KeeperExample/KeeperExample/KeeperExample/SDKViewController.swift
+++ Vendors/KeeperExample/KeeperExample/KeeperExample/SDKViewController.swift
@@ -8,8 +8,11 @@ import RxSwift
 final class SDKViewController: UIViewController {
 
     private var currentServer: Enviroment.Server = .mainNet
-    @IBOutlet private weak var labelInfo: UILabel!
     private let disposeBag = DisposeBag()
+
+    @IBOutlet private weak var labelInfo: UILabel!
+    @IBOutlet private weak var acitivityIndicatorBalance: UIActivityIndicatorView!
+    @IBOutlet private weak var buttonLoadBalance: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,7 @@ final class SDKViewController: UIViewController {
         setupButton()
         labelInfo.text = "Example"
         WavesSDK.initialization(servicesPlugins: .init(data: [], node: [], matcher: []), enviroment: .init(server: currentServer, timestampServerDiff: 0))
+        self.acitivityIndicatorBalance.isHidden = true
     }
 
     @IBAction private func generateNewSeed(_ sender: Any) {
@@ -31,17 +35,27 @@ final class SDKViewController: UIViewController {
         
         let privateKey = PrivateKeyAccount(seedStr: "")
         
-        WavesSDK.shared.services.nodeServices
-        .addressesNodeService
-        .addressBalance(address: privateKey.address)
+        self.buttonLoadBalance.isEnabled = false
+        self.buttonLoadBalance.setTitle(nil, for: .normal)
+        self.acitivityIndicatorBalance.isHidden = false
+        self.acitivityIndicatorBalance.startAnimating()
+        
+        WavesSDK.shared.services
+            .nodeServices
+            .addressesNodeService
+            .addressBalance(address: privateKey.address)
             .observeOn(MainScheduler.instance)
             .subscribe(onNext: { [weak self] (balance) in
+                guard let self = self else { return }
+                self.setupInfo(title: "Balance is:", value: String(balance.balance))
+                self.setupDefaultButtonBalanceState()
+                
+            }, onError: { [weak self] (error) in
                 
                 guard let self = self else { return }
-                
-                self.setupInfo(title: "Balance is:", value: String(balance.balance))
+                self.setupDefaultButtonBalanceState()
+
             }).disposed(by: disposeBag)
-        
     }
     
     @objc private func changeNetwork() {
@@ -57,6 +71,13 @@ final class SDKViewController: UIViewController {
 }
 
 private extension SDKViewController {
+    
+    func setupDefaultButtonBalanceState() {
+        self.acitivityIndicatorBalance.stopAnimating()
+        self.acitivityIndicatorBalance.isHidden = true
+        self.buttonLoadBalance.isEnabled = true
+        self.buttonLoadBalance.setTitle("Load address Waves balance", for: .normal)
+    }
     
     func setupInfo(title: String, value: String) {
         
