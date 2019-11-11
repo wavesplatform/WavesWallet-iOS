d6d04aa01a2e112ef9910a6b8a2099c494ae0e07
Pavel Gubin <xdozorx@gmail.com>
2019-11-11 13:37:51 +0200

    IOS-866

--- WavesWallet-iOS/Modules/Receive/Generate/ReceiveGenerateAddressViewController.swift
+++ WavesWallet-iOS/Modules/Receive/Generate/ReceiveGenerateAddressViewController.swift
@@ -41,6 +41,7 @@ final class ReceiveGenerateAddressViewController: UIViewController {
         navigationItem.backgroundImage = UIImage()
         removeTopBarLine()
         navigationItem.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        navigationItem.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -49,6 +50,7 @@ final class ReceiveGenerateAddressViewController: UIViewController {
         navigationItem.backgroundImage = nil
         showTopBarLine()
         navigationItem.titleTextAttributes = nil
+        navigationItem.largeTitleTextAttributes = nil
     }
     
     private func acceptGeneratingAddress() {
