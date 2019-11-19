ef10b5b023263ecfd8fd94756fdbba7dbe7407b3
Mefilt <Mefilt@gmail.com>
2019-11-19 19:29:42 +0300

    WIP

---
+++ WavesWallet-iOS/Modules/ServerMaintenance/ServerMaintenanceViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ForceUpdateAppViewController.swift
+//  WavesWallet-iOS
+//
+//  Created by Pavel Gubin on 01.11.2019.
+//  Copyright © 2019 Waves Exchange. All rights reserved.
+//
+
+import UIKit
+import WavesSDK
+import DomainLayer
+
+final class ServerMaintenanceViewController: UIViewController {
+
+    @IBOutlet private weak var labelTitle: UILabel!
+    @IBOutlet private weak var labelSubtitle: UILabel!
+            
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLocalization()
+    }
+    
+    private func setupLocalization() {
+        labelTitle.text = Localizable.Waves.ServerMaintenance.Label.title
+        labelSubtitle.text = Localizable.Waves.ServerMaintenance.Label.subtitle
+    }
+}
