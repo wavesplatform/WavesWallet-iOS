d8fadef6523776fb5aec1c297d45c11998bdb2e9
Mefilt <Mefilt@gmail.com>
2020-05-14 20:22:50 +0300

    MWEP-1970 Added new ICON

--- WavesWallet-iOS/Modules/Trade/TradeViewController.swift
+++ WavesWallet-iOS/Modules/Trade/TradeViewController.swift
@@ -55,7 +55,7 @@ final class TradeViewController: UIViewController {
                                                   style: .plain,
                                                   target: self,
                                                   action: #selector(searchTapped))
-        let ordersBarButtonItem = UIBarButtonItem(image: Images.orders.image,
+        let ordersBarButtonItem = UIBarButtonItem(image: Images.deleteAllOrders22.image,
                                                   style: .plain,
                                                   target: self,
                                                   action: #selector(myOrdersTapped))
